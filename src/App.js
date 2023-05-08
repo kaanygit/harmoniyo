12b44f85de2f2cec66961814df6158e826cc832f
kaanygit <yasinkaan345@hotmail.com>
2023-05-08 05:51:35 +0300

    source

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline items-center justify-center flex  font-bold text-5xl ">
+      Harmony Music App
+    </h1>
+  );
+}
+
+export default App;
